1ecc13f7a6e9f188f9acf88b99ec163a347d64b7
Starpact <sjtu_yhj@outlook.com>
2020-10-30 22:23:17 +0800

    补充垂直差值，并与水平差值合并为一维差值

--- src/calculate/preprocess.rs
+++ src/calculate/preprocess.rs
@@ -37,6 +37,20 @@ pub enum InterpMethod {
     TwoDimension,
 }
 
+/// interpolation of temperature matrix
+/// ### Argument:
+/// 2D matrix of the delta temperatures between adjacent frames
+///
+/// positions of thermocouples
+///
+/// interpolation method
+///
+/// position of upper left corner
+///
+/// width of calculation region
+///
+/// ### Return:
+/// 2D matrix of the interpolated temperatures and how a pixel query about its temperature from this matrix
 pub fn interp(
     t2d: ArrayView2<f64>,
     tc_pos: &Vec<(i32, i32)>,
@@ -44,53 +58,53 @@ pub fn interp(
     upper_left_pos: (usize, usize),
     region_shape: (usize, usize),
 ) -> (Array2<f64>, Array1<usize>) {
-    let (cal_h, cal_w) = region_shape;
-    let pix_num = cal_h * cal_w;
     match interp_method {
-        InterpMethod::Horizontal => {
-            let tc_x: Vec<i32> = tc_pos
-                .iter()
-                .map(|pos| pos.1 - upper_left_pos.1 as i32)
-                .collect();
-            let mut query_index = Array1::zeros(pix_num);
-            let mut iter = query_index.iter_mut();
-            for _ in 0..cal_h {
-                for x in 0..cal_w {
-                    *iter.next().unwrap() = x;
-                }
-            }
-            (interp_x(t2d, tc_x, cal_w), query_index)
+        InterpMethod::Horizontal | InterpMethod::Vertical => {
+            interp_1d(t2d, tc_pos, upper_left_pos, interp_method, region_shape)
         }
-        _ => unimplemented!("在做了"),
+        InterpMethod::TwoDimension => unimplemented!("在做了"),
     }
 }
 
-/// *linearly interpolate the temperature along axis **X***
-/// ### Argument:
-/// 2D matrix of the delta temperatures between adjacent frames
-///
-/// relative x positions of thermocouples in the calculation region
-///
-/// width of calculation region
-/// ### Return:
-/// 2D matrix of the interpolated temperatures
-fn interp_x(t2d: ArrayView2<f64>, tc_x: Vec<i32>, cal_w: usize) -> Array2<f64> {
-    let mut interp_x_t2d = Array2::zeros((t2d.nrows(), cal_w));
-
-    par_azip!((row0 in t2d.axis_iter(Axis(0)), mut row1 in interp_x_t2d.axis_iter_mut(Axis(0))) {
+/// *one dimension interpolation, along axis X or Y*
+fn interp_1d(
+    t2d: ArrayView2<f64>,
+    tc_pos: &Vec<(i32, i32)>,
+    ul_pos: (usize, usize),
+    interp_method: InterpMethod,
+    region_shape: (usize, usize),
+) -> (Array2<f64>, Array1<usize>) {
+    let (cal_h, cal_w) = region_shape;
+    let mut query_index = Array1::zeros(cal_h * cal_w);
+    let mut iter = query_index.iter_mut();
+    let mut interp_1d_t: Array2<f64>;
+    let tc_pos_1d: Vec<i32> = match interp_method {
+        InterpMethod::Horizontal => {
+            (0..cal_h).for_each(|_| (0..cal_w).for_each(|x| *iter.next().unwrap() = x));
+            interp_1d_t = Array2::zeros((t2d.nrows(), cal_w));
+            tc_pos.iter().map(|pos| pos.1 - ul_pos.1 as i32).collect()
+        }
+        InterpMethod::Vertical => {
+            (0..cal_h).for_each(|y| (0..cal_w).for_each(|_| *iter.next().unwrap() = y));
+            interp_1d_t = Array2::zeros((t2d.nrows(), cal_h));
+            tc_pos.iter().map(|pos| pos.0 - ul_pos.0 as i32).collect()
+        }
+        _ => panic!("only horizontal or vertical for interpolation 1D"),
+    };
+    let len_of_interp_dimension = interp_1d_t.ncols() as i32;
+    par_azip!((row0 in t2d.axis_iter(Axis(0)), mut row1 in interp_1d_t.axis_iter_mut(Axis(0))) {
         let mut iter = row1.iter_mut();
         let mut curr = 0;
-        let (mut left, mut right) = (tc_x[curr], tc_x[curr + 1]);
-        for i in 0..cal_w as i32 {
-            if i == right && curr + 2 < tc_x.len() {
+        let (mut left, mut right) = (tc_pos_1d[curr], tc_pos_1d[curr + 1]);
+        for pos in 0..len_of_interp_dimension {
+            if pos == right && curr + 2 < tc_pos_1d.len() {
                 curr += 1;
-                left = tc_x[curr];
-                right = tc_x[curr + 1];
+                left = tc_pos_1d[curr];
+                right = tc_pos_1d[curr + 1];
             }
-            *iter.next().unwrap() = (row0[curr] * (right - i) as f64
-                + row0[curr + 1] * (i - left) as f64) / (right - left) as f64;
+            *iter.next().unwrap() = (row0[curr] * (right - pos) as f64
+                + row0[curr + 1] * (pos - left) as f64) / (right -left) as f64;
         }
     });
-
-    interp_x_t2d
+    (interp_1d_t, query_index)
 }
