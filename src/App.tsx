67ecf5cc4d2d4f7f5c3b29aeb4fa476f550d5243
starpact <yhj.await@gmail.com>
2022-06-05 00:19:21 +0800

    tidy dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 import { invoke } from "@tauri-apps/api/tauri";
 
@@ -15,18 +13,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
       <br />
       <button onClick={load_config}>load_config</button>
